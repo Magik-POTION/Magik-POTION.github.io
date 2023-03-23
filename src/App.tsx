bc79ba0a1b5530b506e129bb9cdadb254324fc10
gaiensuit <truong.k77@gmail.com>
2023-03-22 17:07:33 -0700

    Add Background

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,124 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useCallback } from "react";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
+import Particles from "react-particles";
+import type {
+  Container,
+  Engine,
+  IOptions,
+  RecursivePartial,
+} from "tsparticles-engine";
+import { loadFull } from "tsparticles";
 
-function App() {
+/* background by SVGBackgrounds.com */
+
+const styles = StyleSheet.create({
+  view: { flex: 1, flexDirection: "column" },
+  content: {
+    flex: 1,
+    flexDirection: "column",
+  },
+});
+
+const particleOptions: RecursivePartial<IOptions> = {
+  autoPlay: true,
+  background: {
+    color: {
+      value: "#120b12",
+    },
+  },
+  fpsLimit: 144,
+  fullScreen: {
+    enable: true,
+  },
+  interactivity: {
+    events: {
+      onClick: {
+        enable: true,
+        mode: "push",
+      },
+      onHover: {
+        enable: true,
+        mode: "repulse",
+      },
+      resize: true,
+    },
+    modes: {
+      push: {
+        quantity: 4,
+      },
+      repulse: {
+        distance: 200,
+        duration: 0.4,
+      },
+    },
+  },
+  particles: {
+    color: {
+      value: "#ffffff",
+    },
+    links: {
+      color: "#ffffff",
+      distance: 150,
+      enable: true,
+      opacity: 0.5,
+      width: 1,
+    },
+    collisions: {
+      enable: true,
+    },
+    move: {
+      direction: "none",
+      enable: true,
+      outModes: {
+        default: "bounce",
+      },
+      random: false,
+      speed: 2,
+      straight: false,
+    },
+    number: {
+      density: {
+        enable: true,
+        area: 800,
+      },
+      value: 80,
+    },
+    opacity: {
+      value: 0.5,
+    },
+    shape: {
+      type: "circle",
+    },
+    size: {
+      value: { min: 1, max: 5 },
+    },
+  },
+  detectRetina: true,
+};
+
+const App = () => {
+  const particlesInit = useCallback(async (engine: Engine) => {
+    await loadFull(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(async (container?: Container) => {
+    await console.log(container);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Particles
+        style={styles.view}
+        id="tsparticles"
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={particleOptions}
+      />
+      <ScrollView style={styles.content}>
+
+      </ScrollView>
+    </>
   );
-}
+};
 
 export default App;
